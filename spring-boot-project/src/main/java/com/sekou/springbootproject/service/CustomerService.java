f4f1a9ceeabea9b0c2e22d4a964290826e620e1d
Sekou S Seh <sehsekou98@gmail.com>
2023-11-03 02:05:25 +0100

    services

--- spring-boot-project/src/main/java/com/sekou/springbootproject/service/CustomerService.java
+++ spring-boot-project/src/main/java/com/sekou/springbootproject/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.sekou.springbootproject.service;
 
 import com.sekou.springbootproject.customers.Customer;
 import com.sekou.springbootproject.customers.CustomerDb;
+import com.sekou.springbootproject.exception.ResourceNotFound;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -22,7 +23,7 @@ public class CustomerService {
 
     public Customer getCustomer(Integer id) {
         return customerDb.selectCustomerById(id).orElseThrow(
-                () -> new IllegalArgumentException("customer with id [%s] not found".formatted(id))
+                () -> new ResourceNotFound("customer with id [%s] not found".formatted(id))
         );
     }
 }
