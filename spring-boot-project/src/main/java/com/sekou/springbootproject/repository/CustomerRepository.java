729c04a698024b999927e1d63c04da63761f1524
Sekou S Seh <sehsekou98@gmail.com>
2023-11-18 00:47:06 +0100

    Testing

--- spring-boot-project/src/main/java/com/sekou/springbootproject/repository/CustomerRepository.java
+++ spring-boot-project/src/main/java/com/sekou/springbootproject/repository/CustomerRepository.java
@@ -3,6 +3,8 @@ package com.sekou.springbootproject.repository;
 import com.sekou.springbootproject.customers.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
+
 public interface CustomerRepository
         extends JpaRepository<Customer, Integer> {
     boolean existsCustomerByEmail(String email);
